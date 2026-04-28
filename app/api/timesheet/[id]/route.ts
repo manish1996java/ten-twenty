301e9a765791efda9ffd1204c2f3e8de78bbf5f7
Manish Kumar <manishkumar.1996.pj@gmail.com>
2026-04-28 12:07:47 +0530

    change in api route

--- app/api/timesheet/[id]/route.ts
+++ app/api/timesheet/[id]/route.ts
@@ -2,19 +2,20 @@ import { NextResponse } from "next/server";
 import { timesheets } from "@/lib/mockData";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+    req: Request,
+    { params }:  { params: Promise<{ id: string }> } 
 ) {
-  const id = Number(params.id);
+    const { id } = await params;
 
-  const timesheet = timesheets.find((t) => t.id === id);
 
-  if (!timesheet) {
-    return NextResponse.json(
-      { error: "Timesheet not found" },
-      { status: 404 }
-    );
-  }
+    const timesheet = timesheets.find((t) => t.id === Number(id));
 
-  return NextResponse.json(timesheet);
+    if (!timesheet) {
+        return NextResponse.json(
+            { error: "Timesheet not found" },
+            { status: 404 }
+        );
+    }
+
+    return NextResponse.json(timesheet);
 }
